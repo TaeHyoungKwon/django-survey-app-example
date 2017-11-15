c6628a5f3f9da2b3b90d4b8e3598f32f4af1f084
TaeHyoungKwon <kwon5604@naver.com>
2017-11-16 02:06:38 +0900

    Add url patterns and fuction to view each pages

--- app2/polls/views.py
+++ app2/polls/views.py
@@ -1,6 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+def detail(request, question_id):
+    response = "Detail page id : %s"
+    return HttpResponse(response % question_id)
+
 
 def index(request):
     return HttpResponse("<h1>Hello World!</h1>")
+
+
+def result(request, question_id):
+    response = "Question %s"
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("Vote Page: %s" % question_id)
