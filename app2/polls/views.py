6616d314b10162e8462f4a264b58a505d9da4ab7
TaeHyoungKwon <kwon5604@naver.com>
2017-11-16 02:16:54 +0900

    Connect template

--- app2/polls/views.py
+++ app2/polls/views.py
@@ -1,13 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Question
+
 def detail(request, question_id):
     response = "Detail page id : %s"
     return HttpResponse(response % question_id)
 
 
 def index(request):
-    return HttpResponse("<h1>Hello World!</h1>")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = " //// ".join([q.question_text for q in latest_question_list])
+
+    context = {
+        "latest_question_list" : latest_question_list
+    }
+
+    return render(request,"polls/index.html",context)
 
 
 def result(request, question_id):
