60b2abfcc05459bd7d63bf0d97f81183fc9a2eb8
TaeHyoungKwon <kwon5604@naver.com>
2017-11-20 23:43:29 +0900

    Applying generic view

--- app2/polls/urls.py
+++ app2/polls/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import url
 from . import views
+from .views import (
+    IndexView,
+    DetailView,
+    ResultView
+)
 
 urlpatterns = [
-    url(r'^$', views.index, name= 'index'),
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name= 'detail'),
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name= 'results'),
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name= 'vote'),
+    url(r'^$', IndexView.as_view(), name= 'index'),
+    url(r'^(?P<pk>[0-9]+)/$', DetailView.as_view(), name= 'detail'),
+    url(r'^(?P<pk>[0-9]+)/results/$', ResultView.as_view(), name= 'results'),
+    url(r'^(?P<pk>[0-9]+)/vote/$', views.vote, name= 'vote'),
 ]
--- app2/polls/views.py
+++ app2/polls/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView, DetailView
 
 from .models import Question
 
-
+'''
 def detail(request, question_id):
     q = get_object_or_404(Question, pk = question_id)
 
@@ -11,8 +12,19 @@ def detail(request, question_id):
         "question" : q
     }
     return render(request, 'polls/detail.html', context)
+'''
 
+class DetailView(DetailView):
+    model = Question
+    template_name = "polls/detail.html"
 
+class IndexView(ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+'''
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     output = " //// ".join([q.question_text for q in latest_question_list])
@@ -22,16 +34,21 @@ def index(request):
     }
 
     return render(request,"polls/index.html",context)
-
-
+'''
+'''
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
 
     return render(request, 'polls/results.html', {'question': question})
+'''
+
+class ResultView(DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
+def vote(request, pk):
+    question = get_object_or_404(Question, pk=pk)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except:
@@ -42,4 +59,4 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        return redirect('polls:results', question_id=question_id)
+        return redirect('polls:results', pk=pk)
